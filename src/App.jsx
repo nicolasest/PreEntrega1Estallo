6d6ed68cf3b9ae4b91761f07a104cd9e317e07dd
Nicolas Estallo <nicoestallo@gmail.com>
2024-02-06 12:18:35 -0300

    PreEntrega1Estallo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import { Navbar } from './components/layout/NavBar/Navbar'
+import { ItemListContainer } from './components/pages/ItemListContainer/ItemListContainer'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+    <ItemListContainer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import { CartWidget } from "../../common/CartWidget"
+
+
+import './NavBar.css'
+export const Navbar = () => {
+  return (
+    < >
+    <div id='NavBarContainer'>
+      <ul>
+      <li>Hombre</li>
+      <li>Mujer</li>
+      <li>Fitness</li></ul>
+      <CartWidget />
+    </div>
+  
+ 
+    </>
+  )
+}
